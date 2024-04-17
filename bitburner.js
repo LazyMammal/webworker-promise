049075725f744c3201948a9baefc609534e6ecc9
Dennis Carlstrom <LazyMammal@users.noreply.github.com>
2024-04-17 07:34:36 -0400

    Create bitburner.js

---
+++ bitburner.js
@@ -0,0 +1,8 @@
+import { wrap } from 'wrap.js'
+
+/** @param {NS} ns */
+export async function main(ns) {
+  const argList = [1000];
+  let res = await wrap(ns.read('compute.js'), 'calcResult', argList);
+  ns.tprint(res);
+}
