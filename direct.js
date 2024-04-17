f27e0bdc1f70ee51a77bef92e69c4b66dd4808f0
Dennis Carlstrom <LazyMammal@users.noreply.github.com>
2024-04-17 07:36:54 -0400

    Create direct.js

---
+++ direct.js
@@ -0,0 +1,9 @@
+import { calcResult } from 'compute.js'
+
+/** @param {NS} ns */
+export async function main(ns) {
+  const argList = [1000];
+  // let res = await wrap(ns.read('compute.js'), 'calcResult', argList);
+  let res = calcResult(...argList);
+  ns.tprint(res);
+}
